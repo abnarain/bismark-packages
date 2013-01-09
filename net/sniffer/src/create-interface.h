d4efdc69e551837e4b5969fe0aff71e47ee4a97e
abhinav narain <nabhinav3@gatech.edu>
2013-01-09 01:50:29 +0000

    adding /tmp/usr fs

--- net/sniffer/src/create-interface.h
+++
@@ -1,5 +0,0 @@
-int checkup(char* device) ;
-int open_infd(const char device[]);
-int down_radio_interface(const char device[]);
-int up_radio_interface(const char device[]);
-int config_radio_interface(const char device[]);
